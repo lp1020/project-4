7ec9cc102e74b3e469c7d483d0c77e5611075b7b
Liana Penn <lianapenn@gmail.com>
2017-10-10 16:04:53 -0400

    full crud in erb

--- CrueltyFree4MeApp/app/controllers/posts_controller.rb
+++ CrueltyFree4MeApp/app/controllers/posts_controller.rb
@@ -1,23 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
     @category = Category.all
   end
-
+  def show
+    @category = Category.find(@post.category_id)
+  end
   def new
     @post = Post.new
   end
-
+  def all
+    @posts = Post.all
+    @category = Category.all
+  end
   def create
     post = Post.new(post_params)
     post.user = current_user
     post.save!
     redirect_to posts_url
   end
+def edit
 
+end
+def update
+
+  # puts @post
+   #post.update!
+  respond_to do |format|
+    if @post.update(post_params)
+      format.html { redirect_to post_ur, notice: "Post was successfully updated."}
+    else
+      format.html { render :edit }
+    end
+  end
+end
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destoryed."}
+      format.json { head :no_content }
+  end
+end
   private
-
+   def set_post
+    @post = Post.find(params[:id])
+ end
   def post_params
     params.require(:post).permit(:name, :category_id, :is_cruelty_free, :link)
   end
 end
+
--- CrueltyFree4MeApp/config/routes.rb
+++ CrueltyFree4MeApp/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'welcome#index'
+  get '/posts', to: 'posts#all'
+  resources :users do
+resources :posts, only: [:index]
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :posts, only: [:index, :new, :create]
+resources :posts
 end
